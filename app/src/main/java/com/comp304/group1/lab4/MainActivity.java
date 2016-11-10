ae8ad11580f096984847df652c927f429ad91999
Ilmir Taychinov <riml@railgun-games.com>
2016-11-10 01:16:29 -0500

    prepopulated doctors and nurses

--- app/src/main/java/com/comp304/group1/lab4/MainActivity.java
+++ app/src/main/java/com/comp304/group1/lab4/MainActivity.java
@@ -1,6 +1,8 @@
 package com.comp304.group1.lab4;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Bundle;
@@ -30,13 +32,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final DatabaseManager db = new DatabaseManager(this);
+        //db.createDatabase(getApplicationContext());
+        db.dbInitialize( tables,tableCreatorString);
 
         getSupportActionBar().setTitle("Hospital Database (Group1)");
+        prepopulateTable(db);
+
+
     }
 
     public void nurseClick(View v){
         Intent i = new Intent(MainActivity.this, NurseView.class);
         startActivity(i);
+
+    }
+
+    private void prepopulateTable(DatabaseManager db){
+        //pre-populate table
+        SharedPreferences myPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
+        int i = myPrefs.getInt("dbReady", 0); // return 0 if dbReady doesn't exist
+        if(i==0){
+
+            ContentValues values = new ContentValues();
+            String doctorFields[] = {"nurse_id","firstname","lastname","department"};
+            String nurseFields[] = {"doctor_id","firstname","lastname","department"};
+            String doctorRecord1[]={null,"Jacky","Zhang","Human Experiments"};
+            String nurseRecord1[]={null,"Ilmir","Taychinov","Morgue"};
+            String doctorRecord2[]={null,"Josh","Bender","Human Experiments"};
+            String nurseRecord2[]={null,"Konika","Gupta","Human Experiments"};
+
+            db.addRecord(values, "tbl_nurse", nurseFields,nurseRecord1);
+            db.addRecord(values, "tbl_nurse", nurseFields,nurseRecord2);
+            db.addRecord(values, "tbl_doctor", doctorFields,doctorRecord1);
+            db.addRecord(values, "tbl_doctor", doctorFields,doctorRecord2);
+
+            SharedPreferences.Editor e = myPrefs.edit();
+            e.putInt("dbReady", 1); // add or overwrite dbReady
+            e.commit(); // this saves to disk and notifies observers
+        }
 
     }
 }
